80677262d3528a1641771faa81367ff6f15ee95f
sharon-tomer <ttomersharon@gmail.com>
2022-06-22 14:12:19 -0400

    initial commit

---
+++ lib/mongodb.ts
@@ -0,0 +1,36 @@
+import * as mongoDB from "mongodb";
+
+const uri = process.env.MONGODB_URI
+const dbName = process.env.MONGODB_DB
+
+const options = {}
+
+let cachedClient: mongoDB.MongoClient | null = null
+let cachedDb: mongoDB.Db | null = null
+
+export async function connectToDatabase(): Promise<{ client: mongoDB.MongoClient, db: mongoDB.Db }> {
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb }
+  }
+
+  if (!uri) {
+    throw new Error(
+      'Please define the MONGODB_URI environment variable inside .env.local'
+    )
+  }
+  
+  if (!dbName) {
+    throw new Error(
+      'Please define the MONGODB_DB environment variable inside .env.local'
+    )
+  }
+
+  const client = new mongoDB.MongoClient(uri, options);
+
+  const db = await client.db(dbName)
+
+  cachedClient = client
+  cachedDb = db
+
+  return { client, db }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,114 @@
+import Head from 'next/head'
+import {connectToDatabase} from '../lib/mongodb'
+import { GetServerSideProps } from 'next'
+import type { NextPage } from 'next'
+import styles from '../styles/Dashboard.module.css'
+
+const Dashboard: NextPage<DashboardProps> = (props: DashboardProps) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Silk Assignment</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className={styles.title}>
+          Silk Dashboard
+        </h1>
+
+      </main>
+
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async function getServerSideProps() {
+  try {
+    const { db } = await connectToDatabase();
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    const rawDump = await db
+      .collection("raw")
+      .find({})
+      .filter({})
+      // .sort({ metacritic: -1 })
+      // .limit(20)
+      .toArray();
+
+    const groupedDump = await db
+      .collection("grouped")
+      .find({})
+      // .sort({ metacritic: -1 })
+      // .limit(20)
+      .toArray();
+
+    const raw: RawFinding[] = JSON.parse(JSON.stringify(rawDump));
+    const grouped: GroupedFinding[] = JSON.parse(JSON.stringify(groupedDump));
+
+    const groupedFindings: GroupedFindingWithRawData[] = grouped.map((finding: GroupedFinding) => ({
+      ...finding,
+      raw: raw.filter((finding: RawFinding) => finding.grouped_finding_id === finding.id)
+    }))
+
+    return {
+      props: { isConnected: true, groupedFindings},
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
+}
+
+// bad types are bad
+type GroupedFinding = {
+  id: number,
+  description?: string,
+  grouped_finding_created?: string,
+  grouping_key?: string,
+  grouping_type?: string,
+  owner?: string,
+  progress?: number,
+  security_analyst?: string,
+  severity?: string,
+  sla?: string,
+  status?: string,
+  workflow?: string
+}
+
+type GroupedFindingWithRawData = GroupedFinding & { raw: RawFinding[] }
+
+type RawFinding = {
+  grouped_finding_id: number,
+  id: number,
+  asset?: string,
+  description?: string,
+  finding_created?: string,
+  remediation_text?: string,
+  remediation_url?: string,
+  severity?: string,
+  source_collaboration_tool_id?: string,
+  source_collaboration_tool_name?: string,
+  source_security_tool_id?: string,
+  source_security_tool_name?: string,
+  status?: string,
+  ticket_created?: string,
+}
+
+type DashboardProps = {
+  groupedFindings: GroupedFindingWithRawData,
+  isConnected: true
+} | { isConnected: false }
+
+export default Dashboard;
